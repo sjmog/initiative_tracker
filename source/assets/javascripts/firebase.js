7dd8a5284972581f6f4bbab3f014bff5bdcf6cbf
Sam Morgan <s_morgan@me.com>
2017-11-02 20:06:26 +0000

    Add Firebase basic config

---
+++ source/assets/javascripts/firebase.js
@@ -0,0 +1,8 @@
+firebase.initializeApp({
+  apiKey: "AIzaSyDd2Fh4ExYYyscAfn9E5oqfqYvOCnj-xfE",
+  authDomain: "dnd-initiative-tracker.firebaseapp.com",
+  databaseURL: "https://dnd-initiative-tracker.firebaseio.com",
+  projectId: "dnd-initiative-tracker",
+  storageBucket: "",
+  messagingSenderId: "70000993240"
+})
